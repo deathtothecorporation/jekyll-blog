131e4bbf8b8d8cc28e721899d48f32c30f1efc89
Vinney Cavallo <hi@vinneycavallo.com>
2024-06-03 19:47:01 +0000

    Update webhook script for logging

--- sh/github-webhook.sh
+++ sh/github-webhook.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
-echo "Got a github webhook"
+LOG_FILE="/home/vcavallo/vaporware-blog/log/webhook.log"
+
+echo "Webhook received at $(date)" >> "$LOG_FILE"
+
 # Read the request payload from stdin
 payload=$(cat)
+echo "Webhook payload: $payload" >> "$LOG_FILE"
 
 # Extract the ref from the payload
 ref=$(echo "$payload" | grep -oP '"ref":.*?[^\\]"' | cut -d '"' -f 4)
-echo "Got a the payload"
+echo "Ref: $ref" >> "$LOG_FILE"
 
 # Check if the ref matches the desired branch (e.g., 'refs/heads/master')
 if [[ $ref == "refs/heads/master" ]]; then
-    echo "Master branch..."
+    echo "Updating master branch" >> "$LOG_FILE"
     # Navigate to the repository directory
     cd /home/vcavallo/vaporware-blog
 
-    echo "Running post-receive..."
     # Execute the post-receive hook
-    .git/hooks/post-receive
-    echo "Ran post-receive"
+     git pull >> "$LOG_FILE" 2>&1
+    .git/hooks/post-receive >> "$LOG_FILE" 2>&1
+else
+    echo "Ignoring push event for non-master branch" >> "$LOG_FILE"
 fi
-echo "Done!"
+echo "Done! $(date) " >> "$LOG_FILE"
